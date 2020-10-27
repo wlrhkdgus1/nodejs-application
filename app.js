2aa986c8a0f9f6f27b6ee067bfba9bff844611b5
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-10-27 22:33:22 +0900

    1027 express

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 var express = require("express");
 var app = express();
-
+app.use(express.static('public'));
 app.get('/' ,function(req,res){
     res.send('Hello home page');
 });
-
+app.get('/dynamic',function(req,res){
+    var lis = '';
+    for(var i=0; i<5; i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `<!DOCTYPE html>
+    <html lang="UTF-8">
+    <head>
+        <style>
+            h1{
+                color:blue;
+            }
+        </style>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Hello Dynamic</h1>
+        <ul>
+        ${lis}
+        </ul>
+        ${time}
+    </body>
+    </html>`;
+    res.send(output)
+})
+app.get('/route', function(req,res){
+    res.send('Hello Router, <img src="/route.png">')
+})
 app.get('/login',function(req,res){
     res.send('<h1>login plase</h1>');
 });
