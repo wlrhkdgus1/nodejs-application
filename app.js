cf015f979ae74117401b76f90ea4697d842bc196
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-10-30 13:30:42 +0900

    1030 query study

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views','./views');
 app.use(express.static('public'));
+app.get('/topic',function(req,res){
+    var topics = [
+        'Javascript is...',
+        'Nodejs is...',
+        'Express is...'
+    ];
+    var output = `
+      <a href="/topic?id=0">JavaScript</a><br>
+      <a href="/topic?id=1">Nodejs</a><br>
+      <a href="/topic?id=2">Express</a><br><br>
+      ${topics[req.query.id]};
+    `
+    res.send(output);
+})
 app.get('/template',function(req,res){
     res.render('temp',{time:Date(), _title:'Jade'});
 });
