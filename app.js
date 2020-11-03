b6ea28a5810f231a0cab8f49308bf19e0964b65b
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-11-03 23:31:28 +0900

    get and post body-parser 1103

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views','./views');
 app.use(express.static('public'));
-app.get('/topic',function(req,res){
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/form',function(req,res){
+    res.render('form');
+});
+app.get('/form_receiver',function(req,res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+','+description)
+});
+app.post('/form_receiver',function(req,res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+','+description);
+})
+app.get('/topic/:id',function(req,res){
     var topics = [
         'Javascript is...',
         'Nodejs is...',
         'Express is...'
     ];
+     /*query스트링을하려면 req 뒤에 query 시멘틱을 하려면 params*/
     var output = `
-      <a href="/topic?id=0">JavaScript</a><br>
-      <a href="/topic?id=1">Nodejs</a><br>
-      <a href="/topic?id=2">Express</a><br><br>
-      ${topics[req.query.id]};
+      <a href="/topic/0">JavaScript</a><br>
+      <a href="/topic/1">Nodejs</a><br>
+      <a href="/topic/2">Express</a><br><br>
+      ${topics[req.params.id]};
     `
     res.send(output);
 })
+app.get('/topic/:id/:mode',function(req,res){
+    res.send(req.params.id+','+req.params.mode)
+})
+app.get('/')
 app.get('/template',function(req,res){
     res.render('temp',{time:Date(), _title:'Jade'});
 });
